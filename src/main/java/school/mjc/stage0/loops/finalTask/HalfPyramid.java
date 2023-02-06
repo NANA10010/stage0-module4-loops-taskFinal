64777ccd9ef3da9b350dfc51577c1d48f322d710
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-06 20:41:34 +0400

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-                for (int i=1; i<=cathetusLength; i++){
-            for (int j=cathetusLength; j>=i; j--){
+for (int i=1; i<=cathetusLength; i++){
+            for (int j=cathetusLength; j>i; j--){
                 System.out.print(" ");
             }
             for (int j=1; j<=i; j++){
@@ -12,7 +12,6 @@ public class HalfPyramid {
 
             System.out.println();
         }
-
     }
 
     public static void main(String[] args) {
